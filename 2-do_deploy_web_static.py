91b3520ace981844f4e7133910314ac31f078f99
AMINEXD33 <amine.gamer222@gmail.com>
2024-02-01 20:46:09 +0100

    fix

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -17,21 +17,17 @@ def do_deploy(archive_path):
     # check if archive exists
     if not os.path.exists(archive_path):
         return False
-
     try:
-        ar = archive_path.split("/")
-        bs = archive_path[1].strip('.tgz')
-
-        # upload to host
+        arc = archive_path.split("/")
+        base = arc[1].strip('.tgz')
         put(archive_path, '/tmp/')
-        # make a new dir in the hosts
-        sudo(f'mkdir -p /data/web_static/releases/{bs}')
-
-        path = f"/data/web_static/releases/{bs}"
-        sudo('rm /tmp/{}'.format(ar[1]))
-        sudo('mv {}/web_static/* {}/'.format(path, path))
+        sudo('mkdir -p /data/web_static/releases/{}'.format(base))
+        main = "/data/web_static/releases/{}".format(base)
+        sudo('tar -xzf /tmp/{} -C {}/'.format(arc[1], main))
+        sudo('rm /tmp/{}'.format(arc[1]))
+        sudo('mv {}/web_static/* {}/'.format(main, main))
         sudo('rm -rf /data/web_static/current')
-        sudo('ln -s {}/ "/data/web_static/current"'.format(path))
+        sudo('ln -s {}/ "/data/web_static/current"'.format(main))
         return True
     except:
         return False
