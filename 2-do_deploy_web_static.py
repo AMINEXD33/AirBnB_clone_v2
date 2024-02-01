d366b7a1a0c7c7d0f25b3a0cfcb201cc475d0f60
AMINEXD33 <amine.gamer222@gmail.com>
2024-02-01 19:58:25 +0100

    task 3

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,37 @@
+#!/usr/bin.python3
+"""distribute the package to all servers"""
+from fabric.api import *
+import os.path
+
+
+env.username = 'ubuntu'
+env.hosts = ["54.242.176.4", "54.162.88.196"]
+env.key = "~/.ssh/school"
+
+
+def do_deploy(arch):
+    """do_deploy
+    this function deployes the archive passed to all
+    server hosts
+    """
+    # check if archive exists
+    if not os.path.exists(arch):
+        return False
+
+    try:
+        ar = arch.split("/")
+        bs = arch[1].strip('.tgz')
+
+        # upload to host
+        put(arch, '/tmp/')
+        # make a new dir in the hosts
+        sudo(f'mkdir -p /data/web_static/releases/{bs}')
+
+        path = f"/data/web_static/releases/{bs}"
+        sudo('rm /tmp/{}'.format(arc[1]))
+        sudo('mv {}/web_static/* {}/'.format(path, path))
+        sudo('rm -rf /data/web_static/current')
+        sudo('ln -s {}/ "/data/web_static/current"'.format(path))
+        return True
+    except (exception):
+        return False
