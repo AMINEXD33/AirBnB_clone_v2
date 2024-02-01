2d91727ba99a89213635a382bc923f496ea9dc3c
AMINEXD33 <amine.gamer222@gmail.com>
2024-02-01 19:35:46 +0100

    fixed

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,27 +1,23 @@
 #!/usr/bin/python3
-"""
-   just a simple use case of fabric to run some tasks
-"""
-from fabric import task
-import datetime
-
-
-def get_date_formated():
-    """get_date_formated
-    return the string that represent
-    Year month day hour minuts seconds
+"""web server distribution
     """
-    formated = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
-    return (formated)
+from fabric.api import local
+import tarfile
+import os.path
+import re
+from datetime import datetime
 
 
-@task
-def do_pack(c):
+def do_pack():
     """do_pack
     pack the content of the webstatic dir into a .tgz file
     in the versions dir , that this function will also create
     """
-    file_name = f"web_static_{get_date_formated()}.tgz"
-    c.local("mkdir versions")
-    c.local(f"tar -cvzf /versions/{file_name} web_static")
-    return (f"/versions/{file_name}")
+    target = local("mkdir -p versions")
+    name = str(datetime.now()).replace(" ", '')
+    opt = re.sub(r'[^\w\s]', '', name)
+    tar = local('tar -cvzf versions/web_static_{}.tgz web_static'.format(opt))
+    if os.path.exists("./versions/web_static_{}.tgz".format(opt)):
+        return os.path.normpath("/versions/web_static_{}.tgz".format(opt))
+    else:
+        return None
